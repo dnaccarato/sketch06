5986b148ab96fa16ff37c84afc98005b484b20c2
dnaccarato <naccaratoart@gmail.com>
2016-10-25 21:39:36 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 function setup() {
-  createCanvas(800,600);
+  createCanvas(640,360);
 }
 
 function draw() {
-  background(0);
-  fill(255);
-  rect(60,60,70,70);
+  background(201); //one number only because greyscale.
+  stroke(255); // one number only because greyscale.
+  line(118, 170, 99, 285);
+  line(60,280,220,351);
+  line(160,125,210,175);
+  line(230,150,300,140);
+  line(420,225,405,310);
+  line(480,192,520,352);
+  line(500,332,580,360);
+  line(566,105,560,210); 
+  line(638,150,640,315);
+  
+  
 }
